3c82c7b34a44af23d540b36de459b0b2a09389a9
Neil Aldea <njaldea@gmail.com>
2022-12-11 20:47:50 -0500

    [vercel]: update

--- projects/doc/src/routes/4-Internals/2-Controls/+page.svelte
+++ projects/doc/src/routes/4-Internals/2-Controls/+page.svelte
@@ -5,7 +5,9 @@
     import Template from "$lib/components/block/Template.svelte";
 
     const defaults = {
-        v_tuple: [true, 0, "hello", [false, 1, "world"], undefined]
+        v_tuple: [true, 1, "hello", [false, 2, "world"], undefined],
+        params_default: undefined,
+        description: undefined
     };
 </script>
 
@@ -22,62 +24,129 @@ Roles of each controls:
 
 ---
 
-<Block>
-    <Template
-        let:props
-        {defaults}
-    >
-        <br/>
+<div class="column">
+    <Block>
+        <Template
+            let:props
+            {defaults}
+        >
+            <div class="content">
+                <div>{props.description}</div>
+                <div>
 
-        > Default value
+                > Default value
 
-        <pre><code>{JSON.stringify(defaults, null, 4)}</code></pre>
+                </div>
+                <div>
+                    <pre><code>{JSON.stringify({ v_tuple: defaults.v_tuple }, null, 4)}</code></pre>
+                </div>
 
-        > Resolved value
+                <div>
 
-        <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
-    </Template>
-    <Params tag="first"/>
-    <Controls
-        expand
-        props={[
-            {
-                name: "v_tuple",
-                type: "tuple",
-                values: [
-                    {
-                        type: "switch"
-                    },
-                    {
-                        type: "number"
-                    },
-                    {
-                        type: "text"
-                    },
-                    {
-                        type: "tuple",
-                        values: [
-                            {
-                                type: "switch"
-                            },
-                            {
-                                type: "number"
-                            },
-                            {
-                                type: "text"
-                            }
-                        ]
-                    },
-                    {
-                        type: "text"
-                    },
-                ]
-            }
-        ]}
-    />
-</Block>
+                > Params Prop value
+            
+                </div>
+
+                <div>
+                    <pre><code>{JSON.stringify({ v_tuple: props.params_default }, null, 4)}</code></pre>
+                </div>
+
+                <div>
+
+                ---
+
+                </div>
+
+                <div>
+
+                > Resolved value
+
+                </div>
+
+                <div>
+                    <pre><code>{JSON.stringify({ v_tuple: props.v_tuple }, null, 4)}</code></pre>
+                </div>
+            </div>
+        </Template>
+        <Params tag="first" props={{
+            description: "Props not provided to Params"
+        }}/>
+        <Params tag="secod" props={{
+            v_tuple: [
+                false, undefined, "world"
+            ],
+            params_default: [
+                false, undefined, "world"
+            ],
+            description: [
+                "Props partially provided to Params.",
+                "Some are undefined.",
+                "Undefined should follow the Default param values."
+            ].join('\n')
+        }}/>
+        <Controls
+            expand
+            props={[
+                {
+                    name: "v_tuple",
+                    type: "tuple",
+                    values: [
+                        {
+                            type: "switch"
+                        },
+                        {
+                            type: "number"
+                        },
+                        {
+                            type: "text"
+                        },
+                        {
+                            type: "tuple",
+                            values: [
+                                {
+                                    type: "switch"
+                                },
+                                {
+                                    type: "number"
+                                },
+                                {
+                                    type: "text"
+                                }
+                            ]
+                        },
+                        {
+                            type: "text"
+                        },
+                    ]
+                }
+            ]}
+        />
+    </Block>
+</div>
 
 <style>
+    /* hack to change the layout of internal template */
+    /* first global div is coming from Block */
+    /* second global div is from Template */
+    /* by default, Templay layout is just using flex with column direction */
+
+    .column > :global(div > div) {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+    }
+
+    .content {
+        display: grid;
+        grid-template-rows: 90px 50px 310px 50px 170px 50px 50px 1fr;
+        height: 1100px;
+        white-space: pre;
+    }
+
+    .column {
+        height: 150%;
+    }
+
     pre {
         margin: 0;
     }
