d6037e6dfbfaa0893a3a1aff6d62367b098100ba
Neil Aldea <neil.aldea@motional.com>
2022-12-20 09:37:02 -0500

    [gate] minor adjustment to trial

--- projects/gate/src/routes/Trial/+page.svelte
+++ projects/gate/src/routes/Trial/+page.svelte
@@ -7,14 +7,14 @@
     let x2 = 25;
     let y2 = 0;
 
-    let tx = 50;
-    let ty = 50;
+    let tx = 0;
+    let ty = 0;
     let sx = 1;
     let sy = 1;
 </script>
 
 
-<div>
+<div class="svg">
     <SVG>
         <g transform={`translate(${tx} ${ty}) scale(${sx} ${sy})`}>
             <Line bind:x1 bind:y1 bind:x2 bind:y2></Line>
@@ -22,18 +22,36 @@
     </SVG>
 </div>
 
-<input type="range" min={-100} max={100} step={0.001} bind:value={tx}/>
-<input type="range" min={-100} max={100} step={0.001} bind:value={ty}/>
-<input type="range" min={0.001} max={2} step={0.001} bind:value={sx}/>
-<input type="range" min={0.001} max={2} step={0.001} bind:value={sy}/>
-<input type="range" min={0} max={100} step={0.001} bind:value={x1}/>
-<input type="range" min={0} max={100} step={0.001} bind:value={y1}/>
-<input type="range" min={0} max={100} step={0.001} bind:value={x2}/>
-<input type="range" min={0} max={100} step={0.001} bind:value={y2}/>
+<div>
+    <span>translate</span>
+    <input type="range" min={-50} max={50} step={0.001} bind:value={tx}/>
+    <input type="range" min={-50} max={50} step={0.001} bind:value={ty}/>
+</div>
+<div>
+    <span>scale</span>
+    <input type="range" min={0.001} max={2} step={0.001} bind:value={sx}/>
+    <input type="range" min={0.001} max={2} step={0.001} bind:value={sy}/>
+</div>
+<div>
+    <span>start</span>
+    <input type="range" min={-50} max={50} step={0.001} bind:value={x1}/>
+    <input type="range" min={-50} max={50} step={0.001} bind:value={y1}/>
+</div>
+<div>
+    <span>end</span>
+    <input type="range" min={-50} max={50} step={0.001} bind:value={x2}/>
+    <input type="range" min={-50} max={50} step={0.001} bind:value={y2}/>
+</div>
 
 <style>
-    div {
+    .svg {
         width: 500px;
         height: 500px;
+    }
+
+    div:not(.svg) {
+        width: 500px;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
     }
 </style>
