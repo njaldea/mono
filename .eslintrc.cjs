3712d61e5080bc80c92165aed483fe02eed39414
Neil Aldea <njaldea@gmail.com>
2023-01-07 20:07:51 -0500

    [mono] misc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,89 @@
+const ts_recommended = {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-non-null-assertion": "error"
+};
+
+const ts_strict = {
+    "@typescript-eslint/no-unnecessary-condition": "error",
+    "@typescript-eslint/no-throw-literal": "off",
+    "@typescript-eslint/unified-signatures": "off",
+    "@typescript-eslint/array-type": ["error", { default: "array" }],
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"]
+};
+
+const ts_ungrouped = {
+    "@typescript-eslint/consistent-type-exports": "error"
+};
+
+const formatting = {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "semi-spacing": ["error", { before: false, after: true }],
+    "semi-style": ["error", "last"],
+    "no-trailing-spaces": ["error", { skipBlankLines: false, ignoreComments: false }],
+    "no-whitespace-before-property": ["error"],
+    "no-extra-semi": ["error"],
+    "space-before-blocks": ["error", "always"],
+    "keyword-spacing": ["error", { before: true, after: true }],
+    "key-spacing": ["error", { beforeColon: false, afterColon: true, mode: "strict" }]
+};
+
+const enforcement = {
+    eqeqeq: [
+        "error",
+        "always",
+        {
+            null: "ignore"
+        }
+    ],
+    yoda: ["error", "always", { onlyEquality: true }],
+    camelcase: [
+        "error",
+        {
+            properties: "always"
+        }
+    ],
+    "prefer-rest-params": ["error"],
+    "prefer-spread": ["error"],
+    "prefer-template": ["error"],
+    "prefer-const": "error",
+    "block-spacing": ["error", "always"],
+    "padded-blocks": ["error", "never"],
+    "func-style": ["error", "expression"],
+    "consistent-return": ["error"],
+    "dot-notation": ["error"],
+    "require-yield": ["error"],
+    "no-confusing-arrow": ["error"],
+    "no-useless-rename": ["error"],
+    "no-var": ["error"],
+    "no-new": ["error"],
+    "no-nested-ternary": ["error"],
+    "no-multi-assign": ["error"],
+    "no-lonely-if": ["error"],
+    "no-loop-func": ["error"],
+    "no-implicit-globals": ["error"],
+    "no-floating-decimal": ["error"],
+    "no-global-assign": ["error"],
+    "no-extra-boolean-cast": ["error"],
+    "no-extend-native": ["error"],
+    "no-constant-binary-expression": ["error"],
+    "no-unmodified-loop-condition": ["error"],
+    "no-unused-private-class-members": ["error"],
+    "no-use-before-define": ["error"],
+    "no-restricted-syntax": [
+        "error",
+        {
+            selector: "TSEnumDeclaration",
+            message: "Do not use TS Enums"
+        },
+        {
+            selector: ':matches(PropertyDefinition, MethodDefinition)[accessibility="private"]',
+            message: "Use #private instead"
+        }
+    ]
+};
+
 module.exports = {
     root: true,
     parser: "@typescript-eslint/parser",
@@ -10,7 +96,7 @@ module.exports = {
     ],
     plugins: ["svelte3", "@typescript-eslint"],
     ignorePatterns: ["*.cjs"],
-    overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+    overrides: [{ files: ["*.svelte", "*.mdsvelte"], processor: "svelte3/svelte3" }],
     settings: {
         "svelte3/typescript": true
     },
@@ -26,80 +112,10 @@ module.exports = {
         node: true
     },
     rules: {
-        // in eslint recommended
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-non-null-assertion": "error",
-        // in eslint strict
-        "@typescript-eslint/no-unnecessary-condition": "error",
-        "@typescript-eslint/no-throw-literal": "off",
-        "@typescript-eslint/unified-signatures": "off",
-        "@typescript-eslint/array-type": ["error", { default: "array" }],
-        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-        // not in eslint ruleset
-        "@typescript-eslint/consistent-type-exports": "error",
-        eqeqeq: [
-            "error",
-            "always",
-            {
-                null: "ignore"
-            }
-        ],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-        yoda: ["error", "always", { onlyEquality: true }],
-        camelcase: [
-            "error",
-            {
-                properties: "always"
-            }
-        ],
-        "semi-spacing": ["error", { before: false, after: true }],
-        "semi-style": ["error", "last"],
-        "space-before-blocks": ["error", "always"],
-        "keyword-spacing": ["error", { before: true, after: true }],
-        "key-spacing": ["error", { beforeColon: false, afterColon: true, mode: "strict" }],
-        "block-spacing": ["error", "always"],
-        "padded-blocks": ["error", "never"],
-        "func-style": ["error", "expression"],
-        "consistent-return": ["error"],
-        "dot-notation": ["error"],
-        "prefer-rest-params": ["error"],
-        "prefer-spread": ["error"],
-        "prefer-template": ["error"],
-        "prefer-const": "error",
-        "require-yield": ["error"],
-        "no-confusing-arrow": ["error"],
-        "no-useless-rename": ["error"],
-        "no-var": ["error"],
-        "no-new": ["error"],
-        "no-nested-ternary": ["error"],
-        "no-multi-assign": ["error"],
-        "no-lonely-if": ["error"],
-        "no-loop-func": ["error"],
-        "no-implicit-globals": ["error"],
-        "no-floating-decimal": ["error"],
-        "no-global-assign": ["error"],
-        "no-extra-semi": ["error"],
-        "no-extra-boolean-cast": ["error"],
-        "no-extend-native": ["error"],
-        "no-class-assign": ["error"],
-        "no-constant-binary-expression": ["error"],
-        "no-unmodified-loop-condition": ["error"],
-        "no-unused-private-class-members": ["error"],
-        "no-use-before-define": ["error"],
-        "no-trailing-spaces": ["error", { skipBlankLines: false, ignoreComments: false }],
-        "no-whitespace-before-property": ["error"],
-        "no-restricted-syntax": [
-            "error",
-            {
-                selector: "TSEnumDeclaration",
-                message: "Do not use TS Enums"
-            },
-            {
-                selector: ':matches(PropertyDefinition, MethodDefinition)[accessibility="private"]',
-                message: "Use #private instead"
-            }
-        ]
+        ...ts_recommended,
+        ...ts_strict,
+        ...ts_ungrouped,
+        ...formatting,
+        ...enforcement
     }
 };
