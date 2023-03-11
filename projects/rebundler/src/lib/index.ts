f91a297077640bfb9a6bf00ec21fe7a24d8992ed
Neil Aldea <njaldea@gmail.com>
2023-03-11 12:32:06 -0500

    [rebundler][new] v0.0.12 [postpackage][new] v0.0.1

--- projects/rebundler/src/lib/index.ts
+++ projects/rebundler/src/lib/index.ts
@@ -1,2 +1,4 @@
 import type { Config as FullConfig } from "./types/config";
+
 export type Config = Partial<FullConfig>;
+export const defineConfig = (c: Config) => c;
--- projects/rebundler/vite.config.ts
+++ projects/rebundler/vite.config.ts
@@ -1,6 +1,30 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import type { Plugin } from "rollup";
+
+// @ts-expect-error
+import shebang from "rollup-plugin-preserve-shebang";
+
+import { dependencies } from "./package.json";
+
+const sb = shebang as () => Plugin;
 
 export default defineConfig({
-    plugins: [sveltekit()]
+    build: {
+        outDir: "package",
+        lib: {
+            entry: {
+                bin: "./src/bin/index.ts",
+                lib: "./src/lib/index.ts"
+            },
+            formats: ["es"],
+            fileName: (format, entry) => `${entry}/index.js`
+        },
+        target: "node16.15.0",
+        rollupOptions: {
+            external: [...Object.keys(dependencies), "fs", "fs/promises", "path", "process"],
+            plugins: [sb()]
+        }
+    },
+    plugins: [dts()]
 });
