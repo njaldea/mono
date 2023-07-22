f1a175c97d58492956db0f664d1a34a75361bd68
Neil Aldea <njaldea@gmail.com>
2023-07-22 16:46:23 -0400

    [misc] update pnpm and deps

--- projects/jwalk/src/lib/index.ts
+++ projects/jwalk/src/lib/index.ts
@@ -1,21 +1,4 @@
 import { Builder } from "./Builder";
-import type { ActionItem, Action as A } from "./types/actionargs";
-
-export type Action<Context, Value> = (A & { context: Context; value: Value })["output"];
-
-export type TupleActions<Context> = readonly (ActionItem & {
-    context: Context;
-    key: number;
-    type: string;
-    value: any;
-})["output"][];
-
-export type ObjectActions<Context> = readonly (ActionItem & {
-    context: Context;
-    key: string;
-    type: string;
-    value: any;
-})["output"][];
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const jwalker = <Node = any>() => {
