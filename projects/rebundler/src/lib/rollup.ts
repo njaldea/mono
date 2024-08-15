4485793c37d6d2c6da048deb0cd00b3fe205472e
Neil Aldea <njaldea@gmail.com>
2024-08-15 09:54:53 -0400

    [rebundler] added svelte dependency

--- projects/rebundler/src/lib/rollup.ts
+++ projects/rebundler/src/lib/rollup.ts
@@ -10,18 +10,10 @@ export const build = async (config: Config) => {
     if (null != config.mode && "json" !== config.mode.type) {
         const src = join(config.in, config.mode.file);
         const dst = join(config.out, config.mode.file);
-        const build = await rollup({ input: src });
-
-        await build.write({
-            file: dst,
-            format: config.mode.type,
-            name:
-                "iife" === config.mode.type
-                    ? basename(config.mode.file, extname(config.mode.file))
-                    : undefined,
+        const build = await rollup({
+            input: src,
             plugins: (config.mode.plugins ?? [])
                 .map((v) => {
-                    if ("terser" === v) return terser();
                     if ("svelte" === v) return svelte({
                         emitCss: false,
                         onwarn(warning, handler) {
@@ -35,6 +27,18 @@ export const build = async (config: Config) => {
                         }
                     });
                 })
+                .filter(Boolean)
+            });
+
+        await build.write({
+            file: dst,
+            format: config.mode.type,
+            name:
+                "iife" === config.mode.type
+                    ? basename(config.mode.file, extname(config.mode.file))
+                    : undefined,
+            plugins: (config.mode.plugins ?? [])
+                .map((v) => { if ("terser" === v) return terser(); })
                 .filter(Boolean)
         });
 
